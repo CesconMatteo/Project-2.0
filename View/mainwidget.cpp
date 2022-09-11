da68ba6bca7db6dcda4710e3054c4541555fc085
Cescon Matteo <cesconmatteo18@gmail.com>
2022-09-11 11:50:37 +0200

    aggiunto colore di sfondo mainwidget

--- View/mainwidget.cpp
+++ View/mainwidget.cpp
@@ -36,6 +36,9 @@ MainWidget::MainWidget(QWidget *parent) : QWidget(parent) {
 
     tab->addTab(introTab, "Aprire un file");
 
+    QPalette pal = QPalette();
+    pal.setColor(QPalette::Window, QColor(50,50,50));
+    setPalette(pal);
     setAcceptDrops(true);
     setLayout(macroLayout);
 }
